4b08c44947a32c97bef86f7d73e6b396397d2bbc
Zhe Chen <73773398+czhe0603@users.noreply.github.com>
2023-06-22 15:46:02 -0400

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+Rscript -e 'install.packages("shinyjs", repos = "http://cran.rstudio.com/")'
+Rscript -e 'install.packages("shinyscreenshot", repos = "http://cran.rstudio.com/")'
+Rscript -e 'install.packages("tidyverse", repos = "http://cran.rstudio.com/")'
+Rscript -e 'install.packages("raster", repos = "http://cran.rstudio.com/")'
+Rscript -e 'install.packages("gstat", repos = "http://cran.rstudio.com/")'
+Rscript -e 'install.packages("ggpubr", repos = "http://cran.rstudio.com/")'
+Rscript -e 'install.packages("gridExtra", repos = "http://cran.rstudio.com/")'
+Rscript -e 'install.packages("maps", repos = "http://cran.rstudio.com/")'
